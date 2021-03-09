995d7c49a48c5896862047883c7dd4fedc6365ae
先規　勇樹 <yukisaki80@gmail.com>
2021-03-09 11:41:18 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Scouter GitHub Laravel Test
+
+## 開発環境
+|     | バージョン |
+| --- | -------- |
+| PHP | 7.2.34   |
+
+## 開発環境のセットアップ手順
+```
+composer install
+php artisan migrate
+php artisan db:seed
+```
---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+}
